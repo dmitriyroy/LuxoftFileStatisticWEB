359379cbfa211aae1dacfe51a73af7fb291060ea
Дмитрий Рой < @dmitriyroy>
2016-11-09 15:50:57 +0200

    добавление swing - парсинг файла/каталога

--- src/main/java/dmroy/luxoft/MainContext.java
+++ src/main/java/dmroy/luxoft/MainContext.java
@@ -4,9 +4,7 @@ import dmroy.luxoft.dao.FileStatisticDao;
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-import org.springframework.context.support.FileSystemXmlApplicationContext;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.context.support.XmlWebApplicationContext;
 
 /**
  *
@@ -20,15 +18,8 @@ public class MainContext {
 
     public MainContext() {
         if (factory == null) {
-            System.out.println("\n\nMainContext() 234\n\n");
-            System.out.println("mainContext.xml = " + MainContext.class.getSimpleName());
-//            String path = MainContext.class.getResource("mainContext.xml").toString().substring(6);
-//            String path = "src/main/java/dmroy/luxoft/mainContext.xml";
-//            String path = "/mainContext.xml";
-            String path = "d:/!!!_0/mainContext.xml";
-            System.out.println("path = " + path);
-            ApplicationContext context = new FileSystemXmlApplicationContext(path);
-//            ApplicationContext context = new XmlWebApplicationContext();
+            String path = "mainContext.xml";
+            ApplicationContext context = new ClassPathXmlApplicationContext(path);
             factory = (BeanFactory) context;
             MainContext.fileStatisticDao = (FileStatisticDao) factory.getBean("fileStatisticDao");
 
