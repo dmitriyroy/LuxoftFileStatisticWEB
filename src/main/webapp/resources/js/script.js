53ce01420930ceefce1b05d60473c286c29c9a36
Дмитрий Рой < @dmitriyroy>
2016-11-10 11:48:41 +0200

    чистка

--- src/main/webapp/resources/js/script.js
+++ src/main/webapp/resources/js/script.js
@@ -1,10 +1,12 @@
 window.onload = function (){
     document.getElementsByClassName('button_back_area')[0].style.width = document.getElementById("table_center").offsetWidth + "px";
         var d1 = document.getElementById("mainMenuFileList");
-        d1.style.display = "none";
-alert("Прогрузились");
-        var targetID = document.getElementById('mainMenuFileList');
-alert(targetID.src + "   " + targetID.alt);
+//        d1.style.color = "red";
+//alert("Прогрузились");
+//        d1.style.display = "none";
+//        
+//        var targetID = document.getElementById("mainMenuFileList");
+//alert(targetID.src + "   " + targetID.alt);
 
 //    document.getElementById("mainMenuFileList").hover = function(){
 //        var dm_10 = document.getElementById('mainMenuIndex');
